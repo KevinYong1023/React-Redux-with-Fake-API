56fee53d16ab9b7bb9da9959bb26eda85cd2f0f2
kevin10235 <76609560+kevin10235@users.noreply.github.com>
2021-06-13 17:16:12 +0800

    Update productReducer.js

--- src/Redux/Reducers/productReducer.js
+++ src/Redux/Reducers/productReducer.js
@@ -12,11 +12,11 @@ export const productReducer = (state = InitialState,{type, payload}) => {
       }
 }
 
-export const selectProductReducer = (state = {}, {type,payload}) => {
+export const selectProductsReducer = (state = {}, {type,payload}) => {
       switch (type) {
             case ActionTypes.SELECTED_PRODUCT:
                   return {...state, payload}  
-            case ActionTypes.SELECTED_PRODUCT:
+            case ActionTypes.REMOVE_SELECTED_PRODUCT:
                   return {}     
             default:
                   return state;

c885ddab4c59a89fa0ad96ef233c916874859a06
kevin10235 <76609560+kevin10235@users.noreply.github.com>
2021-06-13 17:15:35 +0800

    Update ProductDetail.js

--- src/Containers/ProductDetail.js
+++ src/Containers/ProductDetail.js
@@ -4,34 +4,31 @@ import axios from 'axios'
 import {useDispatch, useSelector} from 'react-redux'
 import  {selectedProduct,removeselectedProduct} from '../Redux/Actions/productActions'
 
-export default function ProductDetail() {
-      const product = useSelector ((state) => state.product);
+const ProductDetail = () => {
       const {productId} = useParams();
-      const dispatch = useDispatch()
+      const dispatch = useDispatch() 
+      let product = useSelector ((state) => state.product);
       const { image, title, price, category, description } = product;
 
-      const fetchProductDetail =async() => {
+      const fetchProductDetail = async (id) => {
         const response = await axios
-        .get(`https://fakestoreapi.com/products/${productId}`)
-       .catch((err)=>{
-             alert("product not found...",err)
-       })
-       dispatch(selectedProduct(response.data))
-       console.log(response.data)
+          .get(`https://fakestoreapi.com/products/${id}`)
+          .catch((err) => {
+            console.log("Err: ", err);
+          });
+        dispatch(selectedProduct(response.data));
       };
 
-      useEffect(() =>{
-            fetchProductDetail();
-            return () =>{
-                  dispatch(removeselectedProduct())
-            }
-      }, [productId])
-
+      useEffect(() => {
+        if (productId && productId !== "") fetchProductDetail(productId);
+        return () => {
+          dispatch(removeselectedProduct());
+        };
+      }, [productId]);
+      
       return (
+        
             <div className="ui grid container">
-              {Object.keys(product).length === 0 ? (
-                <div>...Loading</div>
-              ) : (
                 <div className="ui placeholder segment">
                   <div className="ui two column stackable center aligned grid">
                     <div className="ui vertical divider">AND</div>
@@ -56,7 +53,8 @@ export default function ProductDetail() {
                     </div>
                   </div>
                 </div>
-              )}
             </div>
           );
 }
+
+export default ProductDetail
